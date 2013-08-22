b48872cc20a6a7ebf587376d01da3b2d0d26c709
Henrik Hjelte <henrik@ubuntu.(none)>
2013-08-22 22:42:35 +0200

    Receive panel gui for colors list

--- scripts/desktop/receive-panel.js
+++ scripts/desktop/receive-panel.js
@@ -6,13 +6,37 @@ define([
 ], function ($) {
 
 	var api = {},
+		$addColorButton,
+		$colorCloneRow,
+		addColorRow = function () {
+			var newRow = $colorCloneRow.clone().show();
+			$colorCloneRow.parent().append(newRow);
+		},
+		addColorClick = function (evt) {
+			evt.preventDefault();
+			addColorRow();
+		},
+		hideClick = function (evt) {
+			evt.preventDefault();
+			$(this).closest('.receive-panel__color').remove();
+		},
+		initBindings = function () {
+			$addColorButton.click(addColorClick);
+			api.$el.on('click',
+				'.receive-panel__color__hide-button',
+				hideClick);
+		},
 		render = function () {
 			var html = new EJS({url: 'views/receive.ejs'}).render(),
 				$dom = $(html);
 
+			$addColorButton = $('.receive-panel__add-color-button', $dom).button();
+			$colorCloneRow = $('.receive-panel__clone-row', $dom);
+			
 			api.$el = $dom;
-		};
 
+			initBindings();
+		};
 	
 	api.render = render;
 	api.$el = undefined;
