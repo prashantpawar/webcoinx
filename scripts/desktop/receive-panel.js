b7d9cff875a476a4d0f3a43681d155ccd33d5272
Henrik Hjelte <henrik@ubuntu.(none)>
2013-09-11 23:10:34 +0200

    Receive-panel address in color table

--- scripts/desktop/receive-panel.js
+++ scripts/desktop/receive-panel.js
@@ -1,28 +1,69 @@
 /*jslint */
 /*global define*/
-
+/*global EJS, Bitcoin */
 define([
     "jquery",
 	"desktop/add-color-dialog"
-], function ($, 
+], function ($,
 			 AddColorDialog) {
+	"use strict";
 
-    var updateBalanceForColor = function (application, colorObj, $colorInfoRow) {
-		var colorMan = application.getColorMan(),
-			wallet = application.getWallet(),
-			colorId = colorObj.colorid,
-			color = colorId,
-			v;
-        if (wallet.dirty > 0) {
-            v = '...'; //Progress indicator
-        } else {
-            v = Bitcoin.Util.formatValue(colorMan.s2c(color, wallet.getBalance(color)));
-            if (color) {
-                v = colorMan.btc2color(v, color);
-            }
-        }
-        $('.color-table__color-balance', $colorInfoRow).text(v);
-    };
+	// Color:
+    //         colorid
+    //          "4a1ae0cf40d33e733a380539dd7375c0acdd0db0"
+    //         issues
+    //          [Object { outindex=0, txhash="97f497fb1873c3897192570...402fec1ebf17dba25d4faa9"}]
+    //          
+    //         metahash
+    //          "78fcb770ff733537d65e047aa773366c3154ecc4"
+    //          
+    //         metaprops
+    //          ["unit"]
+    //          
+    //         name
+    //          "c18:kryne"
+    //          
+    //         server
+    //          "http://devel.hz.udoidio.info:8080/"
+    //          
+    //         style
+    //          "genesis"
+    //          
+    //         unit
+    //          "10000"
+	var	colorAtWallet = function (colorId, walletAddress) {
+		var adr = walletAddress;
+		if (colorId) {
+			adr = (colorId + '@' + walletAddress);
+		}
+		return adr;
+	},
+		updateBalanceForColor = function (application, colorObj, $colorInfoRow) {
+			var colorMan = application.getColorMan(),
+				wallet = application.getWallet(),
+				colorId = colorObj.colorid,
+				color = colorId,
+				v,
+				colorAddress = function () {
+					var wallet = application.getWallet(),
+						walletAddress = wallet.getCurAddress().toString();
+					return colorAtWallet(colorId, walletAddress);
+				},
+				addr = colorAddress();
+
+			if (wallet.dirty > 0) {
+				v = '...'; //Progress indicator
+			} else {
+				v = Bitcoin.Util.formatValue(colorMan.s2c(color, wallet.getBalance(color)));
+				if (color) {
+					v = colorMan.btc2color(v, color);
+				}
+			}
+			$('.color-table__color-balance', $colorInfoRow).text(v);
+			$('.color-table__color-address-field', $colorInfoRow)
+				.val(addr)
+				.attr('title', addr);
+		};
 
 	var api = {},
 		application,
@@ -31,7 +72,7 @@ define([
 		$colorCloneRow,
 		refreshColors = function () {
 			$dom.find('.color-table__color').each(function () {
-				color = $(this).data('color');
+				var color = $(this).data('color');
 				if (color) {
 					updateBalanceForColor(application, color, $(this));
 				}
@@ -41,6 +82,7 @@ define([
 			var $newRow = $colorCloneRow.clone(),
 				colorField = $('.color-table__color-name', $newRow);
 			colorField.text(color.name);
+
 			$newRow.show();
 			$newRow.data('color', color);
 			$colorCloneRow.parent().append($newRow);
@@ -82,19 +124,30 @@ define([
 			$dom = $(html);
 			$addColorButton = $('.color-table__add-color-button', $dom).button();
 			$colorCloneRow = $('.color-table__clone-row', $dom);
-			
+
 			api.$el = $dom;
 
-			bitcoinColor = {
+            $dom.on('focus', 
+					'.color-table__color-address-field',
+					function (e) {
+						this.select();
+					})
+				.on('mouseup',
+					'.color-table__color-address-field',
+					function (e) {
+						this.select();
+						e.preventDefault();
+					});
+
+			addColorRow({
 				name: "BTC",
 				colorid: false
-			};
-			addColorRow(bitcoinColor);
+			});
 
 			initBindings();
 			initAppBindings();
 		};
-	
+
 	api.render = render;
 	api.$el = undefined;
 
